907bba5f9ad128543fbdd7edad11ad3b1bf952e2
kno3comma14 <enyert.vinas@gmail.com>
2021-11-22 10:24:52 -0500

    CLJS problem solved. Connection with WASM pending

--- src/tiedup/core.cljs
+++ src/tiedup/core.cljs
@@ -1,7 +1,8 @@
 (ns tiedup.core
     (:require 
               [reagent.core :as reagent :refer [atom]]
-              [reagent.dom :as rd]))
+              [reagent.dom :as rd]
+              [clojure.string :as string]))
 
 (enable-console-print!)
 
@@ -13,37 +14,103 @@
 (defn get-pairs
   "This will be replaced by rust wasm function"
   []
-  [{:left-word "mapache"
-    :right-word "chemical"}
+  [{:left-word "headache"
+    :right-word "chemical"
+    :answer "che"}
    {:left-word "ukelele"
-    :right-word "element"}])
+    :right-word "element"
+    :answer "ele"}
+   {:left-word "dialogue"
+    :right-word "guessing"
+    :answer "gue"}])
 
-(defonce app-state (atom {:text "Hello world!"
-                          :input-pairs (get-pairs)
-                          :first-letter (first available-characters)
+(defonce app-state (atom {:first-letter (first available-characters)
                           :second-letter (get available-characters 1)
-                          :third-character (get available-characters 2)}))
+                          :third-letter (get available-characters 2)
+                          :actual-pair-index 0}))
 
-(defn get-random-pair
-  "This will be replaced by rust wasm function"
-  [pair-index]
-  (let [random-index (rand-int (count (:input-pairs @app-state)))]
-    (if (not= random-index pair-index)
-      random-index
-      (get-random-pair random-index))))
+(defn prepare-left-word
+  [input-word]
+  (subs input-word 0 (- (count input-word) 3)))
+
+(defn prepare-right-word
+  [input-word]
+  (subs input-word 3))
 
 (defn generate-new-character
-  [actual-letter]
-  (let [letter-index (clojure.string/index-of available-characters actual-letter)
-        next-index (mod (+ letter-index 1) number-of-characters)]
-    (get available-characters next-index)))
+  [component operator k]
+  (let [actual-letter (:value (get component 1))
+        letter-index (string/index-of available-characters actual-letter)
+        next-index (if (>= (operator letter-index 1) 0)
+                     (mod (operator letter-index 1) number-of-characters)
+                     (- number-of-characters 1))
+        next-character (get available-characters next-index)]
+    (swap! app-state assoc-in [k] next-character)))
 
-(defn hello-world []
-  [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this and watch it change!"]])
+(defn verify-answer
+  [first-box second-box third-box pair]
+  (let [first-letter (:value (get first-box 1))
+        second-letter (:value (get second-box 1))
+        third-letter (:value (get third-box 1))
+        answer (str first-letter second-letter third-letter)]
+    (= answer (:answer pair))))
 
-(rd/render [hello-world]
+(defn character-change-button
+  [operation component k]
+  (let [operation-symbol (if (= operation +) "+" "-")]
+    [:input {:type "button"
+             :value operation-symbol
+             :on-click #(generate-new-character component operation k)}]))
+
+(defn character-box
+  [value]
+  [:input {:type "text" :value value}])
+
+(defn left-word
+  [left-word]
+  (prepare-left-word left-word))
+
+(defn right-word
+  [right-word]
+  (prepare-right-word right-word))
+
+(defn create-board
+  []
+  (let [first-letter (character-box (:first-letter @app-state))
+        first-letter-minus-button (character-change-button - first-letter :first-letter)
+        first-letter-plus-button (character-change-button + first-letter :first-letter)
+        second-letter (character-box (:second-letter @app-state))
+        second-letter-minus-button (character-change-button - second-letter :second-letter)
+        second-letter-plus-button (character-change-button + second-letter :second-letter)
+        third-letter (character-box (:third-letter @app-state))
+        third-letter-minus-button (character-change-button - third-letter :third-letter)
+        third-letter-plus-button (character-change-button + third-letter :third-letter)
+        pair (get (get-pairs) (:actual-pair-index @app-state))]
+    [:div
+     (left-word (:left-word pair))
+     first-letter-minus-button
+     first-letter
+     first-letter-plus-button
+     second-letter-minus-button
+     second-letter
+     second-letter-plus-button
+     third-letter-minus-button
+     third-letter
+     third-letter-plus-button
+     (right-word (:right-word pair))
+     [:div
+      [:input {:type "button" 
+               :value "Verify!" 
+               :on-click #(if (verify-answer first-letter second-letter third-letter pair)
+                            (do
+                              (if (= (- (count (get-pairs)) 1) (:actual-pair-index @app-state))
+                                (js/alert "Congratulations! You win!")
+                                (do
+                                  (swap! app-state assoc :actual-pair-index (+ (:actual-pair-index @app-state) 1))
+                                  (js/alert "You guessed one word. Good job!"))))
+                            (js/alert "Keep trying!"))}]]]))
+
+(rd/render [create-board]
            (. js/document (getElementById "app")))
 
 (defn on-js-reload []
